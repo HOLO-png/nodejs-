d4de58f4f88ede980f978a448aa2680ef7da82d4
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2023-04-04 22:24:34 +0700

    update

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
-    allowedHeaders: ["secretHeader"],
-    credentials: false,
+    origin: ["http://localhost:3000"],
   },
 });
 
