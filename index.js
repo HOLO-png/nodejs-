d4060de30bbaee605840074f3ce8796fa002cbd1
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2023-04-04 23:02:04 +0700

    update

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ const io = new Server(server, {
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World 2!");
-});
-
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
