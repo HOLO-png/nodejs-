0c71d05e3510dc6f67bcd685316b849e6a2506f6
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2023-04-08 00:11:36 +0700

    updated

--- controllers/driveCtrl.js
+++ controllers/driveCtrl.js
@@ -4,7 +4,15 @@ const driveCtrl = {
   getLeg: async (req, res) => {
     try {
       const drives = await Drives.find();
-      res.json({ drives });
+      res.json(drives[0].status);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  createDrive: async (req, res) => {
+    try {
+      const drives = await Drives.insert({ req });
+      res.json(drives[0].status);
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
--- routes/driveRouter.js
+++ routes/driveRouter.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const authCtrl = require("../controllers/driveCtrl");
 
 router.get("/drive", authCtrl.getLeg);
+// router.post("/drive", authCtrl.getLeg);
 
 module.exports = router;
