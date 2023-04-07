f058f1915c620b4942fab6fa72631d9b95ada321
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2023-04-08 01:31:18 +0700

    updated

--- socketServer.js
+++ socketServer.js
@@ -8,9 +8,15 @@ const EditData = (data, id, call) => {
 };
 
 const SocketServer = (socket) => {
-  console.log(socket.id);
+  socket.on("event", (user) => {
+    console.log(user);
 
-  socket.on("event", "Print event!!!");
+    users.push({
+      id: user._id,
+      socketId: socket.id,
+      followers: user.followers,
+    });
+  });
 
   // Connect - Disconnect
   socket.on("joinUser", (user) => {
@@ -22,28 +28,25 @@ const SocketServer = (socket) => {
   });
 
   socket.on("disconnect", () => {
-    const data = users.find((user) => user.socketId === socket.id);
-    if (data) {
-      const clients = users.filter((user) =>
-        data.followers.find((item) => item._id === user.id)
-      );
-
-      if (clients.length > 0) {
-        clients.forEach((client) => {
-          socket.to(`${client.socketId}`).emit("CheckUserOffline", data.id);
-        });
-      }
-
-      if (data.call) {
-        const callUser = users.find((user) => user.id === data.call);
-        if (callUser) {
-          users = EditData(users, callUser.id, null);
-          socket.to(`${callUser.socketId}`).emit("callerDisconnect");
-        }
-      }
-    }
-
-    users = users.filter((user) => user.socketId !== socket.id);
+    // const data = users.find((user) => user.socketId === socket.id);
+    // if (data) {
+    //   const clients = users.filter((user) =>
+    //     data.followers.find((item) => item._id === user.id)
+    //   );
+    //   if (clients.length > 0) {
+    //     clients.forEach((client) => {
+    //       socket.to(`${client.socketId}`).emit("CheckUserOffline", data.id);
+    //     });
+    //   }
+    //   if (data.call) {
+    //     const callUser = users.find((user) => user.id === data.call);
+    //     if (callUser) {
+    //       users = EditData(users, callUser.id, null);
+    //       socket.to(`${callUser.socketId}`).emit("callerDisconnect");
+    //     }
+    //   }
+    // }
+    // users = users.filter((user) => user.socketId !== socket.id);
   });
 
   // Likes
