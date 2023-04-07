a7e322dcaef92bb76cd43151693ee97e62605dc3
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2023-04-08 01:55:34 +0700

    updated

--- socketServer.js
+++ socketServer.js
@@ -10,9 +10,7 @@ const EditData = (data, id, call) => {
 const SocketServer = (socket) => {
   socket.on("event", (user) => {
     users.push({
-      id: user._id,
-      socketId: socket.id,
-      followers: user.followers,
+      user,
     });
   });
 
@@ -26,28 +24,29 @@ const SocketServer = (socket) => {
   });
 
   socket.on("disconnect", () => {
-    const data = users.find((user) => user.socketId === socket.id);
-    if (data) {
-      const clients = users.filter((user) =>
-        data.followers.find((item) => item._id === user.id)
-      );
+    // const data = users.find((user) => user.socketId === socket.id);
+    // if (data) {
+    //   const clients = users.filter((user) =>
+    //     data.followers.find((item) => item._id === user.id)
+    //   );
 
-      if (clients.length > 0) {
-        clients.forEach((client) => {
-          socket.to(`${client.socketId}`).emit("CheckUserOffline", data.id);
-        });
-      }
+    //   if (clients.length > 0) {
+    //     clients.forEach((client) => {
+    //       socket.to(`${client.socketId}`).emit("CheckUserOffline", data.id);
+    //     });
+    //   }
 
-      if (data.call) {
-        const callUser = users.find((user) => user.id === data.call);
-        if (callUser) {
-          users = EditData(users, callUser.id, null);
-          socket.to(`${callUser.socketId}`).emit("callerDisconnect");
-        }
-      }
-    }
+    //   if (data.call) {
+    //     const callUser = users.find((user) => user.id === data.call);
+    //     if (callUser) {
+    //       users = EditData(users, callUser.id, null);
+    //       socket.to(`${callUser.socketId}`).emit("callerDisconnect");
+    //     }
+    //   }
+    // }
 
-    users = users.filter((user) => user.socketId !== socket.id);
+    // users = users.filter((user) => user.socketId !== socket.id);
+    console.log(users);
   });
 
   // Likes
