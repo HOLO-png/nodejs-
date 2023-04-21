f478e214fa0cd9688005cdb9445ee6195ae58d8f
Tinh1004 <1004tinh@gmail.com>
2023-04-21 23:43:16 +0700

    commit

--- controllers/tokenNotifyCtrl.js
+++ controllers/tokenNotifyCtrl.js
@@ -23,7 +23,6 @@ const tokenNotifyCtrl = {
       const { userId, token } = req.body;
       const tokenExisted = await TokenNotify.findOne({
         token: token,
-        userId: userId,
       });
       if (tokenExisted) {
         return res.status(500).json({ msg: "Token existed." });
